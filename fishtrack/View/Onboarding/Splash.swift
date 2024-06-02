893f41cb1163c7528e50183f211e2eb8d3c5f2ce
Ben <boeckmannben@gmail.com>
2024-06-02 11:19:36 +0200

    Splash Screen added

---
+++ fishtrack/View/Onboarding/Splash.swift
@@ -0,0 +1,18 @@
+//
+//  Splash.swift
+//  fishtrack
+//
+//  Created by Ben Böckmann on 02.06.24.
+//
+
+import SwiftUI
+
+struct Splash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Splash()
+}
